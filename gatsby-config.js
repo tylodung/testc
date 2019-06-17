002726434b4332675a76b1156f754c36f57908ea
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-06-17 10:57:15 +0700

    up

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
         setup(ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata;
           ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-          ret.generator = "Gatlify CMS Starter";
+          ret.generator = "Câu Chuyện Bí Mật";
           return ret;
         },
         query: `
